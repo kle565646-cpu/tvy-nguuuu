03840d97ccf680bb3d197a3548473be77527de7e
kle565646-cpu <kle565646@gmail.com>
2026-05-30 09:23:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,18 +5,16 @@ from discord.ext import commands
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
+intents.message_content = True
 
-bot = commands.Bot(
-    command_prefix="!",
-    intents=intents
-)
-
-@bot.event
-async def on_ready():
-    print(f"Đã đăng nhập: {bot.user}")
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("🏓 Pong!")
+
+@bot.event
+async def on_ready():
+    print(f"Đã đăng nhập: {bot.user}")
 
 bot.run(TOKEN)
